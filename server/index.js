ffd0f02f3bc1f7ea293e961769135815968cb9ca
Haissam <sambhaur@gmail.com>
2022-12-06 17:46:38 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+//let dbConfig = require('./database/db');
+import dbConfig from '../database/db.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db).then(() => {
+console.log('Database successfully connected!')
+},
+error => {
+	console.log('Could not connect to database : ' + error)
+}
+)
+
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
